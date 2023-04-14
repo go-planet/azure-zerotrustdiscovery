b2682d63729ec8cb2c53c108aa3cd3d3288207d8
AJ <95436511+ThisAmericanBeard@users.noreply.github.com>
2023-04-14 14:50:39 -0600

    Update GetADSyncedAzurePrivilegedUsers.ps1
    Added description of the script function

--- GetADSyncedAzurePrivilegedUsers.ps1
+++ GetADSyncedAzurePrivilegedUsers.ps1
@@ -1,4 +1,6 @@
-﻿# Import the Azure AD module
+﻿#Use this script to find AD synced users within the privileged roles in Azure AD. The script returns a CSV file with the AD Synced users and what roles they have been access.
+ 
+ # Import the Azure AD module
 Import-Module AzureAD
 
 # Connect to Azure AD
